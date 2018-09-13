98c40d64a116c1ded9c0e9f8beeaa4b022440687
Asher <habib.asher@gmail.com>
2018-09-13 17:21:29 +1200

    indent build

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ rm *. class # clean project
 javac Calc . java # compile project
 if [ $? -ne 0 ] # if exit code is not 0 javac failed
 then
-	echo " Compile failed !"
-	exit 1
+  echo " Compile failed !"
+  exit 1
 fi
 java -ea Calc # run test
 
